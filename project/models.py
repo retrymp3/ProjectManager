e6dfb26b0d7dc9754df794e0b7c37d5a58e63b33
Milind <milinddinesh@gmail.com>
2023-05-05 13:32:26 +0530

    add project model

--- project/models.py
+++ project/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from backend.models import CustomUser
 
-# Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=150, unique=True)
+    description = models.TextField()
+
+class Contribution(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    user = models.ForeignKey(CustomUser,on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('project','user')
+
