1f596db2e5c538a309bb89da7d7b1fa527de82b9
retrymp3 <retrymp492@gmail.com>
2023-05-05 13:04:32 +0530

    Default files

--- projectManager/settings.py
+++ projectManager/settings.py
@@ -27,8 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-AUTH_USER_MODEL = 'app_name.CustomUser'
-
+AUTH_USER_MODEL = 'backend.CustomUser'
 # Application definition
 
 INSTALLED_APPS = [
@@ -38,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'backend'
 ]
 
 MIDDLEWARE = [
