672e4172fdf1a0015b9b95fd4e17fdbae8e97e02
Anandu P <anandup0122@gmail.com>
2023-05-05 13:24:06 +0530

    Heading

---
+++ frontend/src/pages/landing.tsx
@@ -0,0 +1,9 @@
+import NavBar from "@/components/nav";
+
+export default function Landing() {
+  return (
+    <div className="flex justify-center">
+      <div className="text-xl font-bold mt-3">Project Manager</div>
+    </div>
+  );
+}
